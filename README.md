37ae7111ed5e386aeb41a2dafd694e871bf89b8b
Wavpro <albin.hedwall@gmail.com>
2023-07-03 22:19:29 +0200

    v1 done

---
+++ README.md
@@ -0,0 +1,20 @@
+#### function getGradient(type: string, fadePercentage: number): string;
+   
+#### function getGradientFade(fadePercentage: number): string;
+   
+#### function getGradientAmberFade(fadePercentage: number): string;
+   
+#### function getGradientAcidFade(fadePercentage: number): string;
+   
+#### function isFade(paintIndex: number): boolean;
+   
+#### function isAmberFade(paintIndex: number): boolean;
+   
+#### function isAcidFade(paintIndex: number): boolean;
+   
+#### function isBerriesAndCherries(paintIndex: number): boolean;
+   
+#### function isAnyFade(paintIndex: number): boolean;
+   
+#### function getFadeName(paintIndex: number): string;
+  
---
+++ build/index.d.ts
@@ -0,0 +1,10 @@
+export declare function getGradient(type: string, fadePercentage: number): string;
+export declare function getGradientFade(fadePercentage: number): string;
+export declare function getGradientAmberFade(fadePercentage: number): string;
+export declare function getGradientAcidFade(fadePercentage: number): string;
+export declare function isFade(paintIndex: number): boolean;
+export declare function isAmberFade(paintIndex: number): boolean;
+export declare function isAcidFade(paintIndex: number): boolean;
+export declare function isBerriesAndCherries(paintIndex: number): boolean;
+export declare function isAnyFade(paintIndex: number): boolean;
+export declare function getFadeName(paintIndex: number): string;
